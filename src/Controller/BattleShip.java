d6e77a1e50e699fa285748bab144fba30833a79d
Morteza98 <Morteza_sarvestani@yahoo.com>
2019-06-05 12:36:37 +0430

    the gridpane is created , now i have to set the cells

--- src/Controller/BattleShip.java
+++ src/Controller/BattleShip.java
@@ -1,6 +1,7 @@
 
 package Controller;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,10 +16,8 @@ public class BattleShip extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/View/FXMLDocument.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("/View/FXMLDocument.fxml"));       
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
--- src/Controller/FXMLDocumentController.java
+++ src/Controller/FXMLDocumentController.java
@@ -5,28 +5,42 @@
  */
 package Controller;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
 /**
+ * FXML Controller class
  *
- * @author Morteza Sarvestani
+ * @author Morteza
  */
 public class FXMLDocumentController implements Initializable {
-    
     @FXML
-    private Label label;
-    
+    private GridPane MyFirstGridPane;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button LeaveGameButton;
+    @FXML
+    private Button RestartGameButton;
+
+    @FXML
+    public void clickGrid(javafx.scene.input.MouseEvent event) {
+        Node clickedNode = event.getPickResult().getIntersectedNode();
+        if (clickedNode != MyFirstGridPane) {
+            // click on descendant node
+            Integer colIndex = GridPane.getColumnIndex(clickedNode);
+            Integer rowIndex = GridPane.getRowIndex(clickedNode);
+            System.out.println("Mouse clicked cell: " + colIndex + " And: " + rowIndex);
+        }
     }
     
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
